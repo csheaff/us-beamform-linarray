12f8587706ce4555998a99a083b09ef6d8e235ab
csheaff <claysheaff@protonmail.com>
2020-06-24 07:31:50 -0700

    import h5 data

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,4 @@
+extern crate hdf5;
 use ndarray::{prelude::*, stack, Zip};
 use ndarray_linalg::{norm::Norm, types::Scalar};
 use ndarray_stats::QuantileExt; // this adds basic stat methods to your arrays
@@ -8,8 +9,17 @@ use fftw::types::*;
 use num_integer::Roots;
 use std::f64::consts::PI;
 use std::time::Instant;
+use hdf5::File;
 
+fn get_data(data_path: &str) -> Array3<f64> {
+    let file = hdf5::File::open(data_path).unwrap();
+    let data = file.dataset("dataset_1").unwrap();
+    let data: Array3<f64> = data.read().unwrap();
+    return data
+}
 
 fn main() {
-    println!("Hello, world!");
+    let data_path = "../example_us_bmode_sensor_data.h5";
+    let data = get_data(data_path);
+    println!("{:?}", data.shape())
 }
