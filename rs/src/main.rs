702c959f0fe7cbdad5cc6b575a6d617de4843399
csheaff <claysheaff@protonmail.com>
2020-06-29 07:34:46 -0700

    set up preproc

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -11,6 +11,16 @@ use std::f64::consts::PI;
 use std::time::Instant;
 use hdf5::File;
 
+const SAMPLE_RATE: f64 = 27.72e6;
+const TIME_OFFSET: f64 = 1.33e-6;
+const SPEED_SOUND: f64 = 1540.0;
+const N_TRANSMIT_BEAMS: u32 = 96;
+const N_PROBE_CHANNELS: u32 = 32;
+const TRANSMIT_FREQ: f64 = 1.6e6;
+const TRANSMIT_FOCAL_DEPTH: f64 = 20e-3;
+const ARRAY_PITCH: f64 = 2.0 * 1.8519e-4;
+const REC_LEN: u32 = 1585;
+
 fn get_data(data_path: &str) -> Array3<f64> {
     let file = hdf5::File::open(data_path).unwrap();
     let data = file.dataset("dataset_1").unwrap();
@@ -18,8 +28,30 @@ fn get_data(data_path: &str) -> Array3<f64> {
     return data
 }
 
+
+fn preproc(data: &Array3<f64>, t: &Array1<f64>, xd: &Array1<f64>) -> (Array3<f64>, Array1<f64>) {    
+
+    let filt_ord = 201;
+    let lc = 0.5e6;
+    let uc = 2.5e6;
+    let lc = lc / (SAMPLE_RATE / 2.0);
+    let uc = uc / (SAMPLE_RATE / 2.0);
+
+    
+    let preproc_data = data.clone();
+    let t_shifted = t.clone();
+    (preproc_data, t_shifted)
+}
+								   
+	   
 fn main() {
     let data_path = "../example_us_bmode_sensor_data.h5";
     let data = get_data(data_path);
-    println!("{:?}", data.shape())
+    let t = Array::range(0.0, REC_LEN as f64, 1.0) / SAMPLE_RATE - TIME_OFFSET;
+    let xd = Array::range(0.0, N_PROBE_CHANNELS as f64, 1.0) * ARRAY_PITCH;
+    let xd_max = *xd.max().unwrap();
+    let xd = xd - xd_max / 2.0;
+    let (preproc_data, t_shifted) = preproc(&data, &t, &xd);
+    
+    println!("{:?}", xd_max)
 }
