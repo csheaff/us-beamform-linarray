cfd11bf9df1c5a4b4c38f271fbe5d5ea632a12c7
Clay Sheaff <claysheaff@protonmail.com>
2020-12-13 13:29:56 -0800

    apply envelop detection and decimation

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -26,7 +26,8 @@ const TRANSMIT_FREQ: f64 = 1.6e6;
 const TRANSMIT_FOCAL_DEPTH: f64 = 20e-3;
 const ARRAY_PITCH: f64 = 2.0 * 1.8519e-4;
 const REC_LEN: u32 = 1585;
-const INTERP_FACT: u32 = 4;
+const UPSAMP_FACT: u32 = 4;
+const DECIM_FACT: u32 = 8;
 
 
 fn fft_priv(x: &Array1<c64>, n: usize, sign: Sign) -> Array1<c64> {
@@ -73,7 +74,7 @@ fn preproc(data: &Array3<f64>, t: &Array1<f64>, xd: &Array1<f64>) -> (Array3<f64
 
     // TODO: Set up filter coefs and apod window
 
-    let rec_len_interp = REC_LEN * INTERP_FACT;
+    let rec_len_interp = REC_LEN * UPSAMP_FACT;
     let mut data_interp = Array3::<f64>::zeros((
 	N_TRANSMIT_BEAMS as usize,
 	N_PROBE_CHANNELS as usize,
@@ -87,7 +88,7 @@ fn preproc(data: &Array3<f64>, t: &Array1<f64>, xd: &Array1<f64>) -> (Array3<f64
 	    let mut dsp_vec = waveform.to_owned().into_raw_vec().to_real_time_vec();
 
 	    // interpolate - currently a bug(ish) requiring truncation. See https://github.com/liebharc/basic_dsp/issues/46
-	    dsp_vec.interpolatei(&mut buffer, &RaisedCosineFunction::new(0.35), INTERP_FACT).unwrap();
+	    dsp_vec.interpolatei(&mut buffer, &RaisedCosineFunction::new(0.35), UPSAMP_FACT).unwrap();
 	    let (mut dsp_vec_data, points) = dsp_vec.get();
 	    dsp_vec_data.truncate(points);	    
 	    // let vec: Vec<f64> = dsp_vec.into(); // This also works but, what if you still need to operate on dsp_vec?
@@ -97,7 +98,7 @@ fn preproc(data: &Array3<f64>, t: &Array1<f64>, xd: &Array1<f64>) -> (Array3<f64
 	    waveform_interp.assign(&Array1::from(dsp_vec_data));
 	}
     }
-    let sample_rate = SAMPLE_RATE * INTERP_FACT as f64;
+    let sample_rate = SAMPLE_RATE * UPSAMP_FACT as f64;
     let t_interp = Array::range(0.0, rec_len_interp as f64, 1.0) / sample_rate - TIME_OFFSET;
     (data_interp, t_interp)
 }
@@ -131,7 +132,7 @@ fn beamform_df(data: &Array3<f64>, time: &Array1<f64>, xd: &Array1<f64>) -> Arra
 	let mut slice = prop_dist.slice_mut(s![r as usize, ..]);
 	slice.assign(&dist);
     }
-    let sample_rate = SAMPLE_RATE * INTERP_FACT as f64;
+    let sample_rate = SAMPLE_RATE * UPSAMP_FACT as f64;
     let mut prop_dist_ind = (prop_dist / SPEED_SOUND * sample_rate).mapv(|x| x.round() as usize);
 
     // replace with last index (likely to be of low signal at that location i.e
@@ -189,12 +190,13 @@ fn analytic(waveform: &Array1<f64>, nfft: usize) -> Array1<c64> {
 
 
 fn envelope(waveform: &Array1<f64>) -> Array1<f64> {
-    let nfft = 2048;
+    let nfft = 6340; // length of data
     let env = analytic(&waveform, nfft).mapv(|x| x.abs());
     let env = env.slice(s![..waveform.len()]).to_owned();
 
     env
 }
+
 
 fn log_compress(data: &Array2<f64>, dr: f64) -> Array2<f64> {
     
@@ -206,10 +208,36 @@ fn log_compress(data: &Array2<f64>, dr: f64) -> Array2<f64> {
 }
 
 
-// fn scan_convert() {
+fn scan_convert(img: &Array2<f64>, x: &Array1<f64>, z: &Array1<f64>)
+		-> (Array2<f64>, Array1<f64>, Array1<f64>) {
 
+    // decimate in depth dimensions
+    let img_decim = img.slice(s![.., ..;DECIM_FACT]).into_owned();
+    let z_new = z.slice(s![..;DECIM_FACT]).into_owned();
+    let x_new = x.clone();
+    
+    // make pixels square by interpolating in lateral dimension 
+    // let dz_new = z_new[1] - z_new[0];
+    // let x_new = Array1::<f64>::range(x[0], x[x.len() - 1], dz_new);
+    // let dz_old = z[1] - z[0];
+    // let interp_fact = dz_new / dz_old;
+    // let mut img_sc = Array2::<f64>::zeros((z_new.len(), x_new.len()));
+    // let mut buffer = SingleBuffer::new();
+    // for n in 0..z_new.len() {
 
-// }
+    // 	let horiz_line = img.slice(s![.., n as usize]).into_owned();
+
+    // 	let mut dsp_vec = horiz_line.to_owned().into_raw_vec().to_real_time_vec();
+    // 	dsp_vec.interpolatef(&mut buffer, &RaisedCosineFunction::new(0.35), interp_fact, 0.0, 2048);
+    // 	let (mut dsp_vec_data, points) = dsp_vec.get();
+    // 	dsp_vec_data.truncate(points);
+	
+    // 	let mut horiz_line_new = img_sc.slice(s![.., n as usize]).into_owned();
+    // 	horiz_line_new.assign(&Array1::<f64>::from(dsp_vec_data));
+    // }
+
+    (img_decim, x_new, z_new)
+}
 
 
 fn img_save(img: &Array2<f64>, img_save_path: &Path) {
@@ -237,12 +265,25 @@ fn main() {
 
     let (preproc_data, t_interp) = preproc(&data, &t, &xd);
 
-    let img = beamform_df(&preproc_data, &t_interp, &xd);
+    let data_beamformed = beamform_df(&preproc_data, &t_interp, &xd); // SOMETHING WRONG WITH THIS
+
+    println!("{:?}", data_beamformed);
+
+    let mut img = Array2::<f64>::zeros(data_beamformed.raw_dim());
+    for n in 0..N_TRANSMIT_BEAMS {
+	let a_line = data_beamformed.slice(s![n as usize, ..]).into_owned();
+	let env = envelope(&a_line);
+	let mut img_slice = img.slice_mut(s![n as usize, ..]);
+	img_slice.assign(&env);
+    }
 
     let img_log = log_compress(&img, 35.0);
 
-    let img_save_path = Path::new("./result.png");
-    img_save(&img_log, &img_save_path);
+    let zd = t * SPEED_SOUND / 2.0;
+    let (img_sc, x_sc, z_sc) = scan_convert(&img_log, &xd, &zd);
 
-    // scan conversion
+    let img_save_path = Path::new("./result.png");
+    img_save(&img_sc, &img_save_path);
+
+
 }
